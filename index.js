dac420a73e4e6b54699d1d90abcf14e549f91530
rovaidkhan07 <rovaidkundi@gmail.com>
2025-07-16 17:01:40 -0700

    hello

---
+++ index.js
@@ -0,0 +1,9 @@
+var _a;
+function getcustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+var customer1 = getCustomer(0);
+var Customer2 = getCustomer(0);
+console.log((_a = Customer === null || Customer === void 0 ? void 0 : Customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+var log = null;
+log === null || log === void 0 ? void 0 : log('a');
---
+++ index.ts
@@ -0,0 +1,11 @@
+type Customer = {
+    birthday?: Date
+};
+function getCustomer(id: number): Customer |null |undefined {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+
+let customer2 = getCustomer(0);
+
+console.log(customer?.birthday?.getFullYear());
